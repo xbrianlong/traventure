0732348e5054c73e152861a016773c8f21334162
chloeoxe <chloe.oxe@gmail.com>
2023-03-18 18:22:17 +0800

    nearby place box completed

---
+++ src/components/ItineraryPage/PlaceBox.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="item-wrapper">
+    <div class="detail-wrapper">
+      <div class="image-wrapper">
+        <img src="../../assets/images/gardensbythebay.jpeg" alt="nearby-image" />
+      </div>
+      <span class="detail-title">Gardens By The Bay</span>
+    </div>
+    <v-btn icon="mdi-plus" class="icon-button"></v-btn>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped>
+.item-wrapper {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  border: solid var(--dark-grey-primary) 1px;
+  min-width: 18%;
+  width: 18%;
+  border-radius: 10px;
+  padding: 5px;
+}
+
+.icon-button {
+  background-color: #6a1b9a;
+  margin-right: 5px;
+  color: white;
+}
+
+.detail-wrapper {
+  display: flex;
+  align-items: center;
+}
+
+.detail-title {
+  font-weight: bold;
+  margin-left: 10px;
+  font-size: 16px;
+}
+
+.image-wrapper {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.image-wrapper img {
+  height: 50px;
+  width: 50px;
+  object-fit: cover;
+  border-radius: 5px;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,14 +2,13 @@
   <div class="home">
     <h1>This is a Home page</h1>
     <LocationCard />
+    <PlaceBox />
   </div>
 </template>
 
 <script setup>
-import LocationCard from '../components/ExplorePage/LocationCard.vue';
-
+import LocationCard from '../components/ExplorePage/LocationCard.vue'
+import PlaceBox from '../components/ItineraryPage/PlaceBox.vue'
 </script>
 
-<style>
-
-</style>
+<style></style>

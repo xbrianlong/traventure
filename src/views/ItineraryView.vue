bcb2ce77acea10d40286a6f4adcc36c49b523119
Li Hui Cham <lihuicham@gmail.com>
2023-03-18 18:29:02 +0800

    basic destination item design done

--- src/views/ItineraryView.vue
+++ src/views/ItineraryView.vue
@@ -1,11 +1,13 @@
 <template>
     <div class="view">
+        <DestinationItem />
     </div>
     <RouterView />
 </template>
 
 <script setup>
 import { RouterView } from 'vue-router';
+import DestinationItem from '../components/ItineraryPage/DestinationItem.vue';
 
 </script>
 

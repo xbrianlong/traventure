eeb43f0abead575acd496ee673971623ce3e54ec
Li Hui Cham <lihuicham@gmail.com>
2023-03-18 13:04:03 +0800

    remove location card

--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -1,16 +1,13 @@
 <template>
     <div class="view">
-        <LocationCard />
+
     </div>
 </template>
 
 <script setup>
-import LocationCard from '../components/ExplorePage/LocationCard.vue';
+
 </script>
 
-<style scoped>
-.dashboard-view {
-    background-color: red;
-}
+<style lang="scss" scoped>
 
 </style>

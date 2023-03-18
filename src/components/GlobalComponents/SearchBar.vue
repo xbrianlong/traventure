59de1a3b30f2808eb5678cbe5d20bf3d4c2fee09
faithtanlh <faithtanlihui@gmail.com>
2023-03-18 20:26:29 +0800

    Completed global search bar

---
+++ src/components/GlobalComponents/SearchBar.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="input-container">
+    <v-icon icon="mdi-magnify" id="icon" size="large"></v-icon>
+    <input
+      type="text"
+      id="search-input"
+      placeholder="Search"
+      :value="modelValue"
+      @input="$emit('update:modelValue', $event.target.value)"
+    />
+  </div>
+</template>
+
+<script setup>
+defineProps(['modelValue'])
+defineEmits(['update:modelValue'])
+</script>
+
+<style scoped>
+.input-container {
+  position: relative;
+  background-color: var(--light-grey-primary);
+  padding: 0px 10px 0px 25px;
+  border-radius: 10px;
+  line-height: 52px;
+}
+
+.input-container #icon {
+  position: absolute;
+  color: var(--dark-grey-primary);
+  align-items: center;
+  right: 0;
+  left: 0;
+  top: 0;
+  bottom: 0;
+  margin: auto 0;
+  padding-left: 35px;
+}
+
+.input-container #search-input {
+  line-height: normal;
+  display: inline-block;
+  vertical-align: middle;
+  font-family: 'Roboto', sans-serif;
+  font-weight: 500;
+  font-size: 16px;
+  color: var(--dark-grey-primary);
+  width: 100%;
+  padding-left: 40px;
+}
+
+.input-container input:focus {
+  outline: none;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,15 +1,11 @@
 <template>
   <div class="home">
     <h1>This is a Home page</h1>
-    <LocationCard />
   </div>
 </template>
 
 <script setup>
-import LocationCard from '../components/ExplorePage/LocationCard.vue';
-
 </script>
 
 <style>
-
 </style>

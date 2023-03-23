3f5b102894b0405336cff75f86f041b1c51923c1
faithtanlh <faithtanlihui@gmail.com>
2023-03-23 17:30:57 +0800

    Completed progress bar, typing and slide animations for landing page

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,10 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
+import { RouterView } from 'vue-router'
 </script>
 
 <template>
-  <header>
-    <div class="wrapper">
-      <nav>
-        <RouterLink to="/">Home</RouterLink> | 
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
   <RouterView />
 </template>
 
-<style scoped>
+<style>
 </style>
--- src/views/LandingPageView.vue
+++ src/views/LandingPageView.vue
@@ -1,6 +1,23 @@
 <template>
   <div class="landing-page">
-    <v-btn id="login-btn" @click="showOverlay = !showOverlay"> Login </v-btn>
+    <SwiperProgressBar :progress="img" />
+    <Background :img="img" />
+    <div class="title">
+      <v-container>
+        <Typewriter :placeNum="img" />
+        <h4 class="text-h4 mb-6 font-weight-light text-white">Plan your trip today.</h4>
+        <v-btn
+          class="rounded-lg"
+          id="login-btn"
+          variant="outlined"
+          color="white"
+          size="x-large"
+          @click="showOverlay = !showOverlay"
+        >
+          Login
+        </v-btn>
+      </v-container>
+    </div>
     <Modal
       :showModal="showOverlay"
       @close="showOverlay = !showOverlay"
@@ -11,10 +28,26 @@
 
 <script setup>
 import Modal from '../components/LandingPage/Modal.vue'
+import Background from '../components/LandingPage/Background.vue'
+import Typewriter from '../components/LandingPage/Typewriter.vue'
+import SwiperProgressBar from '../components/LandingPage/SwiperProgressBar.vue'
 import { ref } from 'vue'
 
 const showOverlay = ref(false)
+
+// Every 10s, change typing animation and switch background
+
+const img = ref(0) // img number state shown
+
+setInterval(function () {
+  img.value = (img.value + 1) % 3
+}, 15000)
 </script>
 
-<style>
+<style scoped>
+.title {
+  position: absolute;
+  top: 36%;
+  left: 10%;
+}
 </style>

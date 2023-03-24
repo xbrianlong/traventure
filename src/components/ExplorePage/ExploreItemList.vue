aeef6936d642752cce648a211d8b6d90bf8cda69
Brian Long <brianlongjx@gmail.com>
2023-03-24 22:25:36 +0800

    component after clicking view all button

--- src/components/ExplorePage/ExploreItemList.vue
+++ src/components/ExplorePage/ExploreItemList.vue
@@ -1,7 +1,6 @@
 <template>
-    <v-card
-        width="100vw"
-        class="py-1">
+    <v-card flat
+        class="py-1 mx-0 card">
         <v-container>
             <v-row
                 class="d-flex">
@@ -16,11 +15,23 @@
                         </v-col>
                         <v-col
                             class="px-1 flex-grow-0 flex-shrink-0">
-                            <v-icon class="icon" icon="mdi-heart-outline"></v-icon>
+                            <v-btn
+                            icon 
+                            flat
+                            @click="toggleLike">
+                            <v-icon v-if="like" class="icon" icon="mdi-heart"></v-icon>
+                            <v-icon v-else class="icon" icon="mdi-heart-outline"></v-icon>
+                            </v-btn>
                         </v-col>
                         <v-col
                             class="px-1 flex-grow-0 flex-shrink-0">
-                            <v-icon class="icon" icon="mdi-plus-circle-outline"></v-icon>
+                            <v-btn
+                            icon 
+                            flat
+                            @click="toggleFavourite">
+                            <v-icon v-if="favourite" class="icon" icon="mdi-check"></v-icon>
+                            <v-icon v-else class="icon" icon="mdi-plus-circle-outline"></v-icon>
+                            </v-btn>
                         </v-col>
                     </v-row>
                     <v-chip label size="x-small" class="ma-1 ml-3 font-weight-bold labelname"> Hotel</v-chip>
@@ -36,7 +47,7 @@
                     <v-row class="px-3 py-0 align-center">
                         <v-col
                             class="px-2 py-0 flex-grow-0 flex-shrink-0">
-                            <v-icon class="icon" icon="mdi-heart-outline"></v-icon>
+                            <v-icon class="icon" icon="mdi-star"></v-icon>
                         </v-col>
                         <v-col
                             class="px-2 py-0 flex-grow-1 flex-shrink-0">
@@ -48,11 +59,11 @@
                     <v-row class="px-3 py-0 align-center">
                         <v-col
                             class="px-2 py-0 flex-grow-0 flex-shrink-0">
-                            <v-icon class="icon" icon="mdi-heart-outline"></v-icon>
+                            <v-icon class="icon" icon="mdi-map-marker"></v-icon>
                         </v-col>
                         <v-col
                             class="px-2 py-0 flex-grow-1 flex-shrink-0">
-                            <v-card-text class="pa-0">4.6</v-card-text>
+                            <v-card-text class="pa-0">10 Bayfront Ave, Singapore 018956</v-card-text>
                         </v-col>
                     </v-row>
 
@@ -60,11 +71,13 @@
                     <v-row class="px-3 py-0 align-center">
                         <v-col
                             class="px-2 py-0 flex-grow-0 flex-shrink-0">
-                            <v-icon class="icon" icon="mdi-heart-outline"></v-icon>
+                            <v-icon class="icon" icon="mdi-phone"></v-icon>
                         </v-col>
                         <v-col
                             class="px-2 py-0 flex-grow-1 flex-shrink-0">
-                            <v-card-text class="pa-0">4.6</v-card-text>
+                            <a href="tel:+65 6688 8868">
+                                <v-card-text class="pa-0">+65 6688 8868</v-card-text>
+                            </a>
                         </v-col>
                     </v-row>
 
@@ -72,27 +85,21 @@
                     <v-row class="px-3 py-0 align-center">
                         <v-col
                             class="px-2 py-0 flex-grow-0 flex-shrink-0">
-                            <v-icon class="icon" icon="mdi-heart-outline"></v-icon>
+                            <v-icon class="icon" icon="mdi-web"></v-icon>
                         </v-col>
                         <v-col
                             class="px-2 py-0 flex-grow-1 flex-shrink-0">
-                            <v-card-text class="pa-0">4.6</v-card-text>
+                            <a href="https://www.marinabaysands.com">
+                                <v-card-text class="pa-0">https://www.marinabaysands.com</v-card-text>
+                            </a>
                         </v-col>
                     </v-row>
                 </v-col>
                 <v-col
                     md="4"
                     lg="4"
-                    class="hidden-xs">
-                    <v-img
-                        class="rounded ma-0 pa-0"
-                        max-height="250px"
-                        max-width="250px"
-                        src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
-                        aspect-ratio="1"
-                        cover  
-                    >
-                    </v-img>
+                    class="hidden-xs img-wrapper">
+                    <img class="image" src="https://cdn.vuetifyjs.com/images/cards/docks.jpg">
                 </v-col>
             </v-row>
         </v-container>
@@ -104,6 +111,27 @@
 </script>
 
 <style scoped>
+.card {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    width: var(--expanded-map-width);
+}
+
+.v-btn--size-default {
+    --v-btn-height: 10px;
+}
+
+.img-wrapper {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+.image {
+    max-width: 250px;
+    max-height: 250px;
+    object-fit: cover;
+}
 .labelname {
     background-color: #6A1B9A;
     color: white;

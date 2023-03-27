3097c47d91a320b865856df18ae638ba0e72043c
Li Hui Cham <lihuicham@gmail.com>
2023-03-27 12:44:51 +0800

    remove comments

--- src/views/ItineraryView.vue
+++ src/views/ItineraryView.vue
@@ -1,28 +1,11 @@
 <template>
     <div class="view">
-        <!-- Uncomment to test page -->
-        <!-- <ExpandableMap />
-        <LocationCard />
-        <ItineraryHeader />
-        <DestinationItem />
-        <DestinationItem />
-        <DestinationItem />
-        <DestinationItem />
-        <DestinationItem />
-        <DestinationItem />
-        <DestinationItem />
-        <DestinationItem />
-        <DestinationItem /> -->
     </div>
     <RouterView />
 </template>
 
 <script setup>
 import { RouterView } from 'vue-router';
-import DestinationItem from '../components/ItineraryPage/DestinationItem.vue';
-import ExpandableMap from '../components/GlobalComponents/ExpandableMap.vue';
-import ItineraryHeader from '../components/ItineraryPage/ItineraryHeader.vue';
-import LocationCard from '../components/GlobalComponents/LocationCard.vue';
 
 </script>
 

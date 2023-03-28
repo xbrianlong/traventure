961302b01856b562ce40e2173c4f984d61a02de3
faithtanlh <faithtanlihui@gmail.com>
2023-03-29 00:35:44 +0800

    Integrated GoogleMaps with itinerary page view

--- src/views/ItineraryView.vue
+++ src/views/ItineraryView.vue
@@ -1,42 +1,39 @@
 <template>
-    <TheHeader />
-    <NavigationBar />
-    <div class="view">
-        <ExpandableMap v-show="toggle"/>
-        <div :class="toggle === true ? 'openMap' : 'closeMap'" >
-            <ItineraryHeader @toggleMap=toggleMap() :changeIcon="toggle"/>
-            <DestinationContainer />
-        </div>
+  <TheHeader />
+  <NavigationBar />
+  <div class="view">
+    <GoogleMap v-show="toggle" placeId="ChIJdZOLiiMR2jERxPWrUs9peIg" />
+    <div :class="toggle === true ? 'openMap' : 'closeMap'">
+      <ItineraryHeader @toggleMap="toggleMap()" :changeIcon="toggle" />
+      <DestinationContainer />
     </div>
-    <RouterView />
+  </div>
+  <RouterView />
 </template>
 
 <script setup>
-import TheHeader from '../components/GlobalComponents/TheHeader.vue';
-import NavigationBar from '../components/GlobalComponents/NavigationBar.vue';
-import { RouterView } from 'vue-router';
-import { ref } from 'vue';
-import ItineraryHeader from '../components/ItineraryPage/ItineraryHeader.vue';
-import ExpandableMap from '../components/GlobalComponents/ExpandableMap.vue';
-import DestinationContainer from '../components/ItineraryPage/DestinationContainer.vue';
+import TheHeader from '../components/GlobalComponents/TheHeader.vue'
+import NavigationBar from '../components/GlobalComponents/NavigationBar.vue'
+import { RouterView } from 'vue-router'
+import { ref } from 'vue'
+import ItineraryHeader from '../components/ItineraryPage/ItineraryHeader.vue'
+import GoogleMap from '../components/GlobalComponents/GoogleMap.vue'
+import DestinationContainer from '../components/ItineraryPage/DestinationContainer.vue'
 
 const toggle = ref(true)
 
 function toggleMap() {
-    toggle.value = !toggle.value
-    console.log(toggle.value)
+  toggle.value = !toggle.value
+  //console.log(toggle.value)
 }
-
 </script>
 
 <style scoped>
-
 .openMap {
-    width: var(--expanded-map-width);
+  width: var(--expanded-map-width);
 }
 
 .closeMap {
-    width: 100%;
+  width: 100%;
 }
-
 </style>

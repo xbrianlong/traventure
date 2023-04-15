e64fcda49afe096acb99760a13624d2f1196f883
Li Hui Cham <lihuicham@gmail.com>
2023-04-15 16:21:01 +0800

    add startplanning image

--- src/views/StartPlanningView.vue
+++ src/views/StartPlanningView.vue
@@ -1,9 +1,28 @@
 <template>
+  <TheHeader />
   <div>
-    <StartPlanningButton />
+    <div class="image-wrapper">
+      <img src="../assets/images/startplanning-img.jpg" />
+    </div>
   </div>
 </template>
 
 <script setup>
-import StartPlanningButton from '../components/StartPlanningPage/StartPlanningButton.vue'
+import TheHeader from '../components/GlobalComponents/TheHeader.vue'
 </script>
+
+<style scoped>
+.image-wrapper {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  overflow: hidden;
+}
+
+.image-wrapper img {
+  height: 100vh;
+  width: 100%;
+  object-fit: cover;
+}
+</style>
